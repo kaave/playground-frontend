31ad4827fda85766dda57962b9408f1747160e4f
kaave <abe@framelunch.jp>
2019-03-26 01:21:31 +0900

    eslint: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,18 +24,28 @@ module.exports = {
     jest: true,
   },
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
+    // typeをexportした際にimport/namedがおかしいため外す
+    // 'import/parsers': {
+    //   '@typescript-eslint/parser': ['.ts', '.tsx'],
+    // },
     // importタグの拡張子がらみTS対応
     'import/resolver': {
       // use <root>/tsconfig.json
       typescript: {},
+      webpack: {
+        config: './tools/webpack/production.js',
+      },
     },
+    // importの際にあーだこーだ言われたくないものを
+    'import/core-modules': [],
   },
   rules: {
     // default exportを押す 無効化
     'import/prefer-default-export': 'off',
+    // ~が機能しないため外す
+    'import/no-unresolved': 'off',
+    // クラスメンバーは改行で区切るが、1行の場合はスルー
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
 
     /*
      * typescript
@@ -44,5 +54,17 @@ module.exports = {
     '@typescript-eslint/explicit-member-accessibility': 'off',
     // 関数のexportの並び順をしばる 有効化
     '@typescript-eslint/adjacent-overload-signatures': 'error',
+    // 関数の戻り値を強制 無効化 voidのみ無効にできたら有効にしたいができないので全部OFF
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // interfaceの命名をIはじまりに 無効化 C#じゃないんで
+    '@typescript-eslint/interface-name-prefix': 'off',
+    // 空のinterfaceをしばる 無効化 アクセス修飾子の代わりに使うことがあるんで
+    '@typescript-eslint/no-empty-interface': 'off',
+    // 未使用の変数を警告 無効化 tscではねる
+    '@typescript-eslint/no-unused-vars': 'off',
+    // any禁止 無効化 tscに任せる
+    '@typescript-eslint/no-explicit-any': 'off',
+    // requireを蹴る 無効化 global-requireって設定があるからいらん
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
