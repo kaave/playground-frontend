44dcd3beac98b825ccbf3a8ce6ce96bb7cc3ad1a
kaave <junkjunctions@gmail.com>
2019-04-17 22:29:32 +0900

    common: update grid layout

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,31 +1,8 @@
 module.exports = {
-  extends: ['stylelint-config-recommended-scss'],
+  extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
   ignoreFiles: ['node_modules/**/*', '_template/**/*', '.tmp/**/*', 'assets/**/*', 'build/**/*', 'src/assets/**/*'],
   syntax: 'scss',
   rules: {
-    /*
-     * prettier
-     */
-    'max-line-length': null,
-    indentation: null,
-    'no-extra-semicolons': null,
-    'declaration-block-semicolon-newline-after': null,
-    'declaration-block-semicolon-newline-before': null,
-    'declaration-block-semicolon-space-after': null,
-    'declaration-block-semicolon-space-before': null,
-    'string-quotes': null,
-    'block-closing-brace-empty-line-before': null,
-    'block-closing-brace-newline-after': null,
-    'block-closing-brace-newline-before': null,
-    'block-closing-brace-space-after': null,
-    'block-closing-brace-space-before': null,
-    'block-opening-brace-newline-after': null,
-    'block-opening-brace-newline-before': null,
-    'block-opening-brace-space-after': null,
-    'block-opening-brace-space-before': null,
-    'number-leading-zero': null,
-    'number-no-trailing-zeros': null,
-
     /*
      * Manual
      */
@@ -33,20 +10,11 @@ module.exports = {
     'at-rule-blacklist': ['extend'],
     // コメント記号とコメント本文の間にスペースを共用する 無効化 IntelliJと相性が悪い
     'comment-whitespace-inside': null,
-    // @の前に空行を強制 いくつかのルールは例外
-    'at-rule-empty-line-before': [
-      'always',
-      {
-        except: ['blockless-after-same-name-blockless', 'first-nested', 'inside-block'],
-        ignore: ['after-comment'],
-      },
-    ],
+    // @の前に空行を強制 無効化 ややこしい
+    'at-rule-empty-line-before': null,
     // 複雑すぎる指定はNG ただし属性っぽいものはだいたいOK
     'selector-max-specificity': ['0,2,0', { ignoreSelectors: ['/:.*/', '/-[^-].*/', '/ \\+ /'] }],
     // カンマの後ろにはスペース
     'function-comma-space-after': 'always-single-line',
-
-    // @extendは難しいから近視
-    'at-rule-blacklist': ['extend'],
   },
 };
