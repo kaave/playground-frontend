bd8e63ac1682b670a7714a99733a4531ae6e6d52
kaave <abe@framelunch.jp>
2019-05-01 01:53:36 +0900

    tools: fix ejs

--- tools/gulp/_view.js
+++ tools/gulp/_view.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
 const htmlmin = require('gulp-htmlmin');
 
 const browser = require('./browser');
@@ -9,15 +10,21 @@ const siteConfig = require('../../src/site-config.json');
 
 const params = { ...siteConfig, NODE_ENV: process.env.NODE_ENV };
 
-gulp.task('view', () => gulp.src(conf.view.src)
-  .pipe(plumber())
-  .pipe(ejs(params, {}, { ext: '.html' }))
-  .pipe(gulp.dest(conf.path.dest.development))
-  .pipe(browser.reload({ stream: true }))
+gulp.task('view', () =>
+  gulp
+    .src(conf.view.src)
+    .pipe(plumber())
+    .pipe(ejs(params, {}))
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest(conf.path.dest.development))
+    .pipe(browser.reload({ stream: true })),
 );
 
-gulp.task('view:build', () => gulp.src(conf.view.src)
-  .pipe(ejs(params, {}, { ext: '.html' }))
-  .pipe(htmlmin({ collapseWhitespace: true }))
-  .pipe(gulp.dest(conf.path.dest.production))
+gulp.task('view:build', () =>
+  gulp
+    .src(conf.view.src)
+    .pipe(ejs(params, {}))
+    .pipe(rename({ extname: '.html' }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(conf.path.dest.production)),
 );
