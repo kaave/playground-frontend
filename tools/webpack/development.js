af5de89c5f245601a29967f02686e38004c5f47d
kaave <abe@framelunch.jp>
2019-05-01 02:10:12 +0900

    webpack: add cache-loader

--- tools/webpack/development.js
+++ tools/webpack/development.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 const webpack = require('webpack');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const hash = require('hash-sum');
 
 const conf = require('../config');
 const { entry, output, resolve, rules, plugins, optimization } = require('./base');
+
+const cacheIdentifier = hash([
+  require('typescript/package.json').version,
+  require('ts-loader/package.json').version,
+  require('cache-loader/package.json').version,
+  fs.readFileSync(path.join(process.cwd(), 'tsconfig.json'), 'utf-8'),
+  fs.readFileSync(path.join(__dirname, 'base.js'), 'utf-8'),
+  fs.readFileSync(path.join(__dirname, 'development.js'), 'utf-8'),
+  process.env.NODE_ENV,
+]);
 
 const appendRules = [
   {
@@ -10,9 +23,16 @@ const appendRules = [
     exclude: /node_modules/,
     use: [
       {
+        loader: 'cache-loader',
+        options: {
+          cacheDirectory: path.join(process.cwd(), 'node_modules', '.cache', 'ts-loader'),
+          cacheIdentifier,
+        },
+      },
+      {
         loader: 'ts-loader',
         options: {
-          transpileOnly: true
+          transpileOnly: true,
         },
       },
     ],
@@ -23,25 +43,18 @@ const appendRules = [
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  entry: Object.entries(entry).reduce(
-    (tmp, [key, value]) => {
-      tmp[key] = [
-        `webpack-dev-server/client?http://localhost:${conf.port.webpackDevServer}`,
-        'webpack/hot/only-dev-server',
-        ...(value instanceof Array ? value : [value]),
-      ];
-      return tmp;
-    },
-    {},
-  ),
+  entry: Object.entries(entry).reduce((tmp, [key, value]) => {
+    tmp[key] = [
+      `webpack-dev-server/client?http://localhost:${conf.port.webpackDevServer}`,
+      'webpack/hot/only-dev-server',
+      ...(value instanceof Array ? value : [value]),
+    ];
+    return tmp;
+  }, {}),
   output,
   resolve,
   optimization,
-  plugins: [
-    ...plugins,
-    new webpack.NamedModulesPlugin(),
-    new ForkTsCheckerWebpackPlugin(),
-  ],
+  plugins: [...plugins, new webpack.NamedModulesPlugin(), new ForkTsCheckerWebpackPlugin()],
   module: {
     rules: [...rules, ...appendRules],
   },
